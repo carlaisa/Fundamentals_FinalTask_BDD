a57e13c0e18f9eb5d569862d8b5e56712a9288da
carla.marquez <carlaisabel21@hotmail.es>
2026-02-22 15:10:54 -0400

    Adding folder for BDD, binding, not working yet :(

--- Login/src/obj/Debug/net8.0/Login.AssemblyInfo.cs
+++ Login/src/obj/Debug/net8.0/Login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e16fda2f4ca5fbff1e684e4ce855a12916698e39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0319110070b2d05e9b9872802ef4a5053148fae8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
