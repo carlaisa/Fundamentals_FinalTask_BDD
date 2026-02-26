8f4f277af679d3e697d08e2f659c21dd05bf8240
carla.marquez <carlaisabel21@hotmail.es>
2026-02-26 16:42:56 -0400

    Advances in BDD, have errors in Then

--- Login/src/Features/login.feature
+++ Login/src/Features/login.feature
@@ -2,19 +2,20 @@ Feature: User Login
 
   Scenario: Login form with empty credentials
     Given the user is in the login page
-    And the user types the username
-    And the user types the password
+    And the user types an invalid username
+    And the user types an invalid password
     When the user clears the username field
     And the user clears the password field
-    And clicks the login button
+    And the user clicks the login button
     Then an error message should be displayed: "Username is required"
 
     Scenario: Login form with credentials by passing username
     Given the user is in the login page
-    And the user types any credentials in username field
-    And the user types the password
-    When the user clear the password field
-    And the user hit the login button
+    And the user types an invalid username
+    And the user types a valid password
+    When the user clears the password field
+    And the user clicks the login button
+    Then an error message should be displayed: "Password is required"
 
     Scenario: Login form with credentials by passing username and password
     Given the user is in the login page
--- Login/src/Steps/LoginSteps.cs
+++ Login/src/Steps/LoginSteps.cs
@@ -10,8 +10,96 @@ namespace PageObject.Steps
     public class LoginSteps
     {
         private readonly ScenarioContext _scenarioContext;
+        private readonly Browser _browser;
         private IWebDriver? _driver;
         private LoginPage _loginPage;
+
+        private IWebDriver Driver =>
+        _driver ?? throw new InvalidOperationException("WebDriver is not initialized.");
+
+        public LoginSteps(Browser browser) => _browser = browser;
+
+        public LoginSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            _driver = DriverFactory.Create(_browser);
+            _driver.Manage().Window.Maximize();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            _driver?.Quit();
+        }
+
+        [Given(@"the user is in the login page")]
+        public void GivenTheUserIsInTheLoginPage()
+        {
+            var loginPage = new PageObject.Pages.LoginPage(_driver);
+            loginPage.Open();
+        }
+
+        [Given(@"the user types an invalid username")]
+        public void GivenTheUserTypesAnInvalidUsername()
+        {
+            var loginPage = new PageObject.Pages.LoginPage(_driver);
+            loginPage.EnterUsername("any_username");
+        }
+
+        [Given(@"the user types an invalid password")]
+        public void GivenTheUserTypesAnInvalidPassword()
+        {
+            var loginPage = new PageObject.Pages.LoginPage(_driver);
+            loginPage.EnterPassword("password123");
+        }
+
+        [Given(@"the user types a valid username")]
+        public void GivenTheUserTypesAValidUsername()
+        {
+            var loginPage = new PageObject.Pages.LoginPage(_driver);
+            loginPage.EnterUsername("standard_user");
+        }
+
+        [Given(@"the user types a valid password")]
+        public void GivenTheUserTypesAValidPassword()
+        {
+            var loginPage = new PageObject.Pages.LoginPage(_driver);
+            loginPage.EnterPassword("secret_sauce");
+        }
+
+        [When(@"the user clears the username field")]
+        public void WhenTheUserClearsTheUsernameField()
+        {
+            var loginPage = new PageObject.Pages.LoginPage(_driver);
+            loginPage.ClearUsername();
+        }
+
+        [When(@"the user clears the password field")]
+        public void WhenTheUserClearsThePasswordField()
+        {
+            var loginPage = new PageObject.Pages.LoginPage(_driver);
+            loginPage.ClearPassword();
+        }
+
+        [When(@"the user clicks the login button")]
+        public void WhenTheUserClicksTheLoginButton()
+        {
+            var loginPage = new PageObject.Pages.LoginPage(_driver);
+            loginPage.ClickLogin();
+        }
+
+        [Then("@an error message should be displayed: ""(.*)""")]
+        public void ThenAnErrorMessageShouldBeDisplayed(string expectedMessage)
+        {
+            var actual = _loginPage.GetErrorMessageText();
+            Assert.AreEqual(expectedMessage, actual);
+        }
     }
 
 
